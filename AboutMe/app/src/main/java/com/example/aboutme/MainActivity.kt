5f35f15e689f8e113ca7b52af6dde5f59dda3a64
Mateus Santos <m.santos.azeve@gmail.com>
2022-03-07 15:14:46 -0300

    retirado comentarios e linhas desnecessarios

--- AboutMe/app/src/main/java/com/example/aboutme/MainActivity.kt
+++ AboutMe/app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -5,9 +5,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import com.example.aboutme.databinding.ActivityMainBinding
 
@@ -17,25 +14,14 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // setContentView(R.layout.activity_main)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        // findViewById<Button>(R.id.done_button).setOnClickListener {
-        //     addNickname(it)
-        // }
         binding.doneButton.setOnClickListener {
             addNickname(it)
         }
     }
 
     private fun addNickname(view: View) {
-        // val editText = findViewById<EditText>(R.id.nickname_edit)
-        // val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
-
-        // nicknameTextView.text = editText.text
-        // editText.visibility = View.GONE
-        // view.visibility = View.GONE
-        // nicknameTextView.visibility = View.VISIBLE
         binding.apply {
             nicknameText.text = binding.nicknameEdit.text
             invalidateAll()
